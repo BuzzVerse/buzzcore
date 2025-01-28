7f3f28b52a4dde00c879ac1a886d23f786adc10f
wybran <krystian.wybranowski@gmail.com>
2025-01-28 17:04:19 +0100

    Init project

---
+++ src/main/java/dev/buzzverse/buzzcore/BuzzCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.buzzverse.buzzcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuzzCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/buzzverse/buzzcore/BuzzCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.buzzverse.buzzcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzzCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
