39629391a674b7fe6e8f725a74f5433fcd0aeb0d
wybran <krystian.wybranowski@gmail.com>
2025-03-25 18:21:16 +0100

    remove test

--- src/test/java/dev/buzzverse/buzzcore/BuzzCoreApplicationTests.java
+++ src/test/java/dev/buzzverse/buzzcore/BuzzCoreApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.buzzverse.buzzcore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BuzzCoreApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
