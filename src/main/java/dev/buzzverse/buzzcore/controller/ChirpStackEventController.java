b5280cf745232747fc6656b838033e9cd23eea99
wybran <krystian.wybranowski@gmail.com>
2025-03-27 17:52:45 +0100

    handle status event

--- src/main/java/dev/buzzverse/buzzcore/controller/ChirpStackEventController.java
+++ src/main/java/dev/buzzverse/buzzcore/controller/ChirpStackEventController.java
@@ -1,5 +1,6 @@
 package dev.buzzverse.buzzcore.controller;
 
+import dev.buzzverse.buzzcore.model.ChirpStackEvent;
 import dev.buzzverse.buzzcore.service.ChirpStackEventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -13,8 +14,7 @@ class ChirpStackEventController {
     private final ChirpStackEventService chirpStackEventService;
 
     @PostMapping(consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public void handleEvent(@RequestParam String event, @RequestBody byte[] requestBody) {
-        System.out.println("Received event: " + event);
-        chirpStackEventService.handleUplinkEvent(requestBody);
+    public void handleEvent(@RequestParam ChirpStackEvent event, @RequestBody byte[] requestBody) {
+        chirpStackEventService.handleEvent(event, requestBody);
     }
 }
--- src/main/java/dev/buzzverse/buzzcore/service/ChirpStackEventService.java
+++ src/main/java/dev/buzzverse/buzzcore/service/ChirpStackEventService.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import dev.buzzverse.buzzcore.BuzzVerse_V1_BME280.BME280Data;
+import dev.buzzverse.buzzcore.BuzzVerse_V1_Chirpstack.StatusEvent;
 import dev.buzzverse.buzzcore.BuzzVerse_V1_Chirpstack.UplinkEvent;
 import dev.buzzverse.buzzcore.BuzzVerse_V1_Chirpstack.UplinkRxInfo;
 import dev.buzzverse.buzzcore.BuzzVerse_V1_Packet.Packet;
+import dev.buzzverse.buzzcore.model.ChirpStackEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,7 +24,26 @@ public class ChirpStackEventService {
 
     private final MqttClientService mqttClientService;
 
-    public void handleUplinkEvent(byte[] requestBody) {
+    public void handleEvent(ChirpStackEvent event, byte[] requestBody) {
+        try {
+            switch (event) {
+                case UP:
+                    handleUplinkEvent(requestBody);
+                    break;
+                case STATUS:
+                    handleStatusEvent(requestBody);
+                    break;
+                default:
+                    log.error("Unknown event type: {}", event);
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unknown event type");
+            }
+        } catch (Exception e) {
+            log.error("Error while handling event", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error while handling event");
+        }
+    }
+
+    private void handleUplinkEvent(byte[] requestBody) {
         try {
             UplinkEvent uplinkEvent = UplinkEvent.parseFrom(requestBody);
             String devEui = uplinkEvent.getDeviceInfo().getDevEui();
@@ -40,6 +61,27 @@ public class ChirpStackEventService {
                 BME280Data bme280Data = packet.getBme280();
                 processAndPublish(bme280Data, rssi, snr, devEui);
             }
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error while parsing protobuf", e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error while parsing protobuf");
+        }
+    }
+
+
+    private void handleStatusEvent(byte[] requestBody) {
+        try {
+            StatusEvent statusEvent = StatusEvent.parseFrom(requestBody);
+            String devEui = statusEvent.getDeviceInfo().getDevEui();
+
+            Boolean isExternalPower = statusEvent.getExternalPowerSource();
+            Boolean isBatteryLevelUnavailable = statusEvent.getBatteryLevelUnavailable();
+            Float battery = statusEvent.getBatteryLevel();
+
+            log.info("Received status event from device: {}", devEui);
+            log.info("External power source: {}", isExternalPower);
+            log.info("Battery level unavailable: {}", isBatteryLevelUnavailable);
+            log.info("Battery level: {}", battery);
 
         } catch (InvalidProtocolBufferException e) {
             log.error("Error while parsing protobuf", e);
