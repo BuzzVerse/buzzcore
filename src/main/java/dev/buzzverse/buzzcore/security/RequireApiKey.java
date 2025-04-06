17ab0ebcf3634b059e5bc070ed6861f994f238ad
wybran <krystian.wybranowski@gmail.com>
2025-04-06 15:55:20 +0200

    refactor: @RequireApiKey annotation instead of ApiKeyAuthFilter

---
+++ src/main/java/dev/buzzverse/buzzcore/security/RequireApiKey.java
@@ -0,0 +1,10 @@
+package dev.buzzverse.buzzcore.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequireApiKey {}
